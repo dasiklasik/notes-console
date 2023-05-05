f549630205224e0d4344855d989edec902dcb369
dasiklasik <daryasamsonowicz@gmail.com>
2023-05-05 13:54:47 +0300

    feat: add list function

--- script.js
+++ script.js
@@ -17,8 +17,21 @@ function create(title, content) {
   });
 }
 
+function list() {
+  fs.readFile('notes.json', (error, data) => {
+    if (error) return console.error(error.message);
+    const notes = JSON.parse(data);
+    if(notes.length === 0) console.log('Заметки еще не созданы');
+    notes.forEach((value, index) => {
+      const str = `${index + 1}. ${value.title}: ${value.content}`;
+      console.log(str)
+    })
+  })
+}
+
 switch (command) {
   case 'list':
+    list();
     break;
   case 'view':
     break;
